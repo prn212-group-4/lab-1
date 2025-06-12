5484df3a0f7a870e6abb54aae8f3a9607a036b79
namdang-fdp <namdangcoder@gmail.com>
2025-06-12 23:05:11 +0700

    feat: add branch

---
+++ LAB1-master-edit/Repositories/ProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using BusinessObjects;
+using System.Collections.Generic;
+using DataAccessLayer;
+
+namespace Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ProductDAO _productDAO = new ProductDAO();
+
+        public void DeleteProduct(Product p) => _productDAO.DeleteProduct(p);
+        public void SaveProduct(Product p) => _productDAO.SaveProduct(p);
+        public void UpdateProduct(Product p) => _productDAO.UpdateProduct(p);
+        public List<Product> GetProducts() => _productDAO.GetProduct();
+        public Product GetProductById(int id) => _productDAO.GetProductById(id);
+    }
+}
---
+++ LAB1-master-edit/Services/CategoryService.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Repositories;
+using BusinessObjects;
+
+namespace Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly ICategoryRepository iCategoryRepository;
+
+        public CategoryService()
+        {
+            iCategoryRepository = new CategoryRepository();
+        }
+
+        public List<Category> GetCategories()
+        {
+            return iCategoryRepository.GetCategories();
+        }
+    }
+}
